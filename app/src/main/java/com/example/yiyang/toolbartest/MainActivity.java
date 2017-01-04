53c7bf56f3222c7949af84bdd554632c557faa9e
chiangyiyang <chiangyiyang@gmail.com>
2017-01-04 11:31:20 +0800

    Toolbar 隱藏及顯示

--- app/src/main/java/com/example/yiyang/toolbartest/MainActivity.java
+++ app/src/main/java/com/example/yiyang/toolbartest/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.yiyang.toolbartest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+
+        final ToggleButton tbnHideShowToolbar = (ToggleButton) findViewById(R.id.tbnHideShowToolbar);
+        tbnHideShowToolbar.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (tbnHideShowToolbar.isChecked())
+                    getSupportActionBar().hide();
+                else
+                    getSupportActionBar().show();
+            }
+        });
     }
 }
